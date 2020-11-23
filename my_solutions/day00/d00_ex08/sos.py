89a90ffc506e641e56906b2fb47018c49272aab7
vinicius111andrade <vinicius111andrade@gmail.com>
2020-11-23 11:59:18 -0300

    new d00_ex08

---
+++ my_solutions/day00/d00_ex08/sos.py
@@ -0,0 +1,59 @@
+'''
+You will have to make a function which encodes strings into Morse code.
+The input will accept all alphanumeric characters.
+'''
+
+import sys
+
+int_morse = {
+'a' : '.-',
+'b' : '-...',
+'c' : '-.-.',
+'d' : '-..',
+'e' : '.',
+'f' : '..-.',
+'g' : '--.',
+'h' : '....',
+'i' : '..',
+'j' : '.---',
+'k' : '-.-',
+'l' : '.-..',
+'m' : '--',
+'n' : '-.',
+'o' : '---',
+'p' : '.--.',
+'q' : '--.-',
+'r' : '.-.',
+'s' : '...',
+'t' : '-',
+'u' : '..-',
+'v' : '...-',
+'w' : '.--',
+'x' : '-..-',
+'y' : '-.--',
+'z' : '--..',
+'0' : '-----',
+'1' : '.----',
+'2' : '..---',
+'3' : '...--',
+'4' : '....-',
+'5' : '.....',
+'6' : '-....',
+'7' : '--...',
+'8' : '---..',
+'9' : '----.'
+}
+
+# check if only alphanum
+# find first word, 69 is a word
+# iterate word chars
+# translate char to morse
+# build morse string
+# print word
+# is there another word?
+# yes, print " / " and go next
+
+def main():
+
+
+main()
