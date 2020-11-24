436b0e3f4ac1a0ccf880542d25e1b96ea8a7392d
vinicius111andrade <vinicius111andrade@gmail.com>
2020-11-24 12:38:06 -0300

    almost done with sos

--- my_solutions/day00/d00_ex08/sos.py
+++ my_solutions/day00/d00_ex08/sos.py
@@ -5,33 +5,59 @@ The input will accept all alphanumeric characters.
 
 import sys
 
-int_morse = {
+int_morse_dict = {
 'a' : '.-',
+'A' : '.-',
 'b' : '-...',
+'B' : '-...',
 'c' : '-.-.',
+'C' : '-.-.',
 'd' : '-..',
+'D' : '-..',
 'e' : '.',
+'E' : '.',
 'f' : '..-.',
+'F' : '..-.',
 'g' : '--.',
+'G' : '--.',
 'h' : '....',
+'H' : '....',
 'i' : '..',
+'I' : '..',
 'j' : '.---',
+'J' : '.---',
 'k' : '-.-',
+'K' : '-.-',
 'l' : '.-..',
+'L' : '.-..',
 'm' : '--',
+'M' : '--',
 'n' : '-.',
+'N' : '-.',
 'o' : '---',
+'O' : '---',
 'p' : '.--.',
+'P' : '.--.',
 'q' : '--.-',
+'Q' : '--.-',
 'r' : '.-.',
+'R' : '.-.',
 's' : '...',
+'S' : '...',
 't' : '-',
+'T' : '-',
 'u' : '..-',
+'U' : '..-',
 'v' : '...-',
+'V' : '...-',
 'w' : '.--',
+'W' : '.--',
 'x' : '-..-',
+'X' : '-..-',
 'y' : '-.--',
+'Y' : '-.--',
 'z' : '--..',
+'Z' : '--..',
 '0' : '-----',
 '1' : '.----',
 '2' : '..---',
@@ -53,7 +79,62 @@ int_morse = {
 # is there another word?
 # yes, print " / " and go next
 
+def is_alphanum_or_space(string):
+	for i in range(len(string)):
+		if not (string[i].isalnum() or string[i] == " "):
+			return False
+	return True
+
+def translate_to_morse(arg_list):
+	translated_list = []
+	new_word = ""
+	char = ""
+	morse_chars = ""
+#need to add a space between morse chars
+	for string in arg_list:
+		str_len = len(string)
+		i = 0
+		while i < str_len:
+			if string[i] == " ":
+				i += 1
+			else:
+				while i < str_len and string[i].isalnum():
+					char = string[i]
+					morse_chars = int_morse_dict[char]
+					for m_char in morse_chars:
+						new_word += m_char
+					i += 1
+				translated_list.append(new_word)
+				new_word = ""
+	return translated_list
+
+def output_translation(words_list):
+	list_len = len(words_list)
+	i = 0
+
+	while i < list_len:
+		if i == 0:
+			print(words_list[i], sep='', end='')
+		elif i == list_len - 1:
+			print(words_list[i], sep='', end='')
+		else:
+			print(" / ", sep='', end='')
+			print(words_list[i], sep='', end='')
+			print(" / ", sep='', end='')
+		i += 1
+
+
 def main():
+	if len(sys.argv) == 1:
+		print("Give me a alphanumeric string to translate!")
+		return 0
+	for item in sys.argv[1:]:
+		if not is_alphanum_or_space(item):
+			print("Give me only alphanumeric strings to translate!")
+			return 0
+	translated_words_list = translate_to_morse(sys.argv[1:])
+	output_translation(translated_words_list)
+
 
 
 main()
